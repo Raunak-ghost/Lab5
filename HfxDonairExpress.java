5e6d1a8a44fdbcc9465573c43ad1a2b19dd57225
Matt Amy <matt.e.amy@gmail.com>
2021-02-08 21:54:47 -0400

    Initial commit

---
+++ HfxDonairExpress.java
@@ -0,0 +1,25 @@
+/**
+ * This program is used by HfxDonairExpress to take their online orders. Their menu is as follows
+ *
+ * Donair:
+ *      small - $5
+ *      med   - $6
+ *      large - $7
+ *
+ * Pizza:
+ *      small - $8
+ *      med   - $9
+ *      large - $10
+ *
+ *      Toppings:
+ *          pepperoni - $1.00
+ *          jalapeno  - $0.99
+ *          mushroom  - $0.75
+ */
+public class HfxDonairExpress {
+
+    public static void main(String[] args) {
+        /* STUB METHOD */
+        System.out.println("Under construction");
+    }
+}
